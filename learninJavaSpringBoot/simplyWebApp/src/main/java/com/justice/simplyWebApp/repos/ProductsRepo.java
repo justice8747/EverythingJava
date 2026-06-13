32036f67650af3c7908d017af41f8765c6a21573
justice8747 <justiceduruanyim8747@gmail.com>
2026-06-13 04:34:15 +0100

    Updated Folder/Repo

--- learninJavaSpringBoot/simplyWebApp/src/main/java/com/justice/simplyWebApp/repos/ProductsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.justice.simplyWebApp.repos;
-
-import com.justice.simplyWebApp.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductsRepo extends JpaRepository<Product, Integer> {
-}
